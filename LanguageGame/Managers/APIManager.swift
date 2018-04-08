6bd68f02d9ac1fe358f6e33c505c6ce62c37b50a
Ayman <ayman.karram@gmail.com>
2018-04-08 15:05:04 +0200

    Impament getsentencesList API service in API manager

--- LanguageGame/Managers/APIManager.swift
+++ LanguageGame/Managers/APIManager.swift
@@ -9,4 +9,21 @@
 import Foundation
 import Alamofire
 
-
+class APIManager {
+    
+    //MARK: URLS
+    struct URLS {
+        static let baseURL = "https://gist.githubusercontent.com/DroidCoder/7ac6cdb4bf5e032f4c737aaafe659b33/raw/baa9fe0d586082d85db71f346e2b039c580c5804/words.json"
+        static let sentencesURL = baseURL
+    }
+    
+    //MARK: API Requests
+    class func getsentencesList(completionHandler: @escaping (DataResponse<[SentenceModel]>)-> Void)  {
+        
+        let url = URLS.sentencesURL
+        _ = request(url, method: .get).responseCollection{ (response: DataResponse<[SentenceModel]>) in
+            
+            completionHandler(response)
+        }
+    }
+}
