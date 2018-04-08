2f734bded752a33ac097fcc3bb108d7b3ece1be9
Ayman <ayman.karram@gmail.com>
2018-04-08 14:58:03 +0200

    Create StartGameViewController.swift to be the first start controller view for the game app

---
+++ LanguageGame/Controllers/StartGameViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StartGameViewController.swift
+//  LanguageGame
+//
+//  Created by Ayman  on 08.04.18.
+//  Copyright © 2018 Ayman . All rights reserved.
+//
+
+import Foundation
+import UIKit
+class StartGameViewController: UIViewController {
+    
+}
