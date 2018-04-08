1610d8501441007274b392a9f4c75e350b0a574a
Ayman <ayman.karram@gmail.com>
2018-04-08 20:21:50 +0200

    Added constants file

---
+++ LanguageGame/Helpers/constants.swift
@@ -0,0 +1,16 @@
+//
+//  constants.swift
+//  LanguageGame
+//
+//  Created by Ayman  on 08.04.18.
+//  Copyright © 2018 Ayman . All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//MARK:- Colors
+let MAINTEXTGRAYCOLOR = UIColor(rgb: (66,66,66))
+
+//MARK:- FONTS
+let MAINFONTNAME = "MarkerFelt-Thin"
