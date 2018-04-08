f4626620ad310ab3ec2d54349ba8be1326822e1a
Ayman <ayman.karram@gmail.com>
2018-04-08 23:39:38 +0200

    Implemet the reacting methods with GameLogicManager in PlayViewController

--- LanguageGame/Controllers/PlayViewController.swift
+++ LanguageGame/Controllers/PlayViewController.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 import TCProgressBar
 import WCLShineButton
+import SwiftMessages
 
 class PlayViewController: UIViewController {
     
@@ -18,17 +19,26 @@ class PlayViewController: UIViewController {
     @IBOutlet weak var correctButton: WCLShineButton!
     @IBOutlet weak var wrongButton: WCLShineButton!
     @IBOutlet weak var sentenceTrackView: UIView!
+    @IBOutlet weak var sentenceLabel: UILabel!
+    
+    // score view
+    @IBOutlet weak var numberOfWrongScoreLabel: UILabel!
+    @IBOutlet weak var numberOfNoAnswerScoreLabel: UILabel!
+    @IBOutlet weak var numberOfCorrectScoreLabel: UILabel!
     
     var sentenceDropView :SentenceDropView?
     
     var playManager = GameLogicManager.sharedInstance
     var sentencesList : [SentenceModel]?
     
+    var userChoiceAnswer = false
+    
     //MARK:- View Life cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpView()
         initVariables()
+        startPlay()
         // Do any additional setup after loading the view.
     }
     
@@ -39,7 +49,6 @@ class PlayViewController: UIViewController {
     
     //MARK:- SetUp Methods
     func setUpView () {
-        
         var choicesButtonsParams = WCLShineParams()
         choicesButtonsParams.bigShineColor = UIColor(rgb: (225,147,0))
         choicesButtonsParams.smallShineColor = UIColor.green
@@ -53,7 +62,6 @@ class PlayViewController: UIViewController {
         wrongButton.image = .like
         
         wrongButton.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
-        gameProgressBar.value = 0.5
     }
     
     func initVariables() {
@@ -61,18 +69,108 @@ class PlayViewController: UIViewController {
         let sentenceDropViewFrame = CGRect(x: 0, y: 0, width: self.sentenceTrackView.frame.size.width, height: self.sentenceTrackView.frame.size.height)
         sentenceDropView = SentenceDropView(frame: sentenceDropViewFrame)
         self.sentenceTrackView.addSubview(sentenceDropView!)
-        sentenceDropView?.startDropLabelWith(text: "Testing", with: {
-            
+    }
+    
+    //MARK:- Play Methods
+    func startPlay () {
+        playManager.setUpGame()
+        setUpRound()
+    }
+    
+    func setUpRound () {
+        self.userChoiceAnswer = false
+        self.enableChoicesButtons(enable: true)
+        self.sentenceLabel.text = playManager.currrentFirstLanguageSentences
+        self.sentenceDropView?.startDropLabelWith(text: playManager.currrentSecondLanguageSentences!, withCompletion: {
+            if self.userChoiceAnswer == false {
+                let _ = self.playManager.userDidSelect(choiceType: .noAnswer)
+                self.updateUserScore()
+            }
         })
+    }
+    
+    func prepareForNextRound () {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.correctButton.isSelected = false
+            self.wrongButton.isSelected = false
+            if self.playManager.userFinishHisGame() == false {
+                self.playManager.prepareNextRound()
+                self.setUpRound()
+            }
+            else
+            {
+                AlertManager.showFinishGameAlertWith(userScore: self.playManager.userScore, buttonTapHandler: { _ in
+                    self.resetGame()
+                    SwiftMessages.hide()
+                    
+                })
+            }
+        }
+    }
+    func showAlertAccordingToUserAnswerWith (choiceType : UserChoices) {
+        switch choiceType {
+        case .correct:
+            AlertManager.showYourAnswerCorrectAlert()
+            break
+        case .wrong:
+            AlertManager.showYourAnswerIsWrongAlertWith(correctAnswer: playManager.getCorrectAnswer())
+            break
+        default:
+            return
+        }
+    }
+    
+    func updateUserScore () {
+        self.numberOfCorrectScoreLabel.text = String (self.playManager.userScore.correct)
+        self.numberOfWrongScoreLabel.text = String (self.playManager.userScore.wrongs)
+        self.numberOfNoAnswerScoreLabel.text = String (self.playManager.userScore.noAnswer)
+        updateGameProgressBar()
+        prepareForNextRound()
+    }
+    
+    func updateGameProgressBar () {
+        self.gameProgressBar.value = CGFloat(playManager.currentRoundIndex + 1) / CGFloat(playManager.totalRoundsCount)
+    }
+    
+    func resetGame() {
+        playManager.resetGame()
+        self.userChoiceAnswer = false
+        self.gameProgressBar.value = 0
+        self.sentenceDropView?.resetLabelPosition()
+        self.resetUserScoreView()
+        self.startPlay()
+    }
+    
+    //MARK:- Helper
+    func enableChoicesButtons(enable : Bool) {
+        self.correctButton.isEnabled = enable
+        self.wrongButton.isEnabled   = enable
+    }
+    
+    func resetUserScoreView () {
+        self.numberOfCorrectScoreLabel.text = "0"
+        self.numberOfWrongScoreLabel.text = "0"
+        self.numberOfNoAnswerScoreLabel.text = "0"
     }
     
     //MARK:- IBAction Methods
     @IBAction func correctButtonClicked(_ sender: Any) {
-        
+        self.userChoiceAnswer = true
+        self.sentenceDropView?.resetLabelPosition()
+        let result = self.playManager.userDidSelect(choiceType: .correct)
+        self.showAlertAccordingToUserAnswerWith(choiceType: result)
+        updateUserScore()
+        self.enableChoicesButtons(enable: false)
     }
     
     @IBAction func wrongButtonClicked(_ sender: Any) {
-        
+        self.userChoiceAnswer = true
+        self.sentenceDropView?.resetLabelPosition()
+        let result = self.playManager.userDidSelect(choiceType: .wrong)
+        self.showAlertAccordingToUserAnswerWith(choiceType: result)
+        updateUserScore()
+        self.enableChoicesButtons(enable: false)
+
     }
     
     @IBAction func dissmissButtonClicked(_ sender: Any) {
